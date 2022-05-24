29839a1ba8d3e5d3efabe8b67a6999d027cfb62d
PretzelOptician <alekschultz1@gmail.com>
2022-05-24 17:07:47 -0400

    initial commit

---
+++ src/ClassPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ClassPage extends React.Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ClassPage
