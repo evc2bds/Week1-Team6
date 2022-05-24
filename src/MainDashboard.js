254dbbb73429c8e987fa8eafbedf9372417babdc
PretzelOptician <alekschultz1@gmail.com>
2022-05-24 13:25:06 -0400

    Set up MainDashboard component

---
+++ src/MainDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class MainDashboard extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>this is the main dashboard</p>
+            </div>
+        );
+    }
+}
+
+export default MainDashboard
