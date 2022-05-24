02b97edcc6904dbae3e9e5493dc1d2cac507ebce
evc2bds <evc2bds@virginia.edu>
2022-05-24 17:37:44 -0400

    not completed teacherDashboard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import './App.css';
-import MainDashboard from "./MainDashboard.js"
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore";
-
+import TeacherDashboard from "./Components/TeacherDashboard";
 
 function App() {
-  const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId
-  };
-  const firebaseApp = initializeApp(firebaseConfig);
-  const db = getFirestore(firebaseApp)
-
 
   return (
-    <div className="App">
-      <MainDashboard />
-    </div>
+    <>
+      <TeacherDashboard />
+    </>
+    // <div className="App">
+      
+    // </div>
   );
 }
 
---
+++ src/Components/TeacherDashboard.js
@@ -0,0 +1,74 @@
+import React, {useState, useEffect, useRef} from 'react'
+import AllTeachers from "./AllTeachers.js"
+import { getFirestore, collection, addDoc, doc, getDocs, updateDoc, deleteDoc } from "firebase/firestore";
+import db from "../firebase.js";
+
+function TeacherDashboard() {
+    //Firebase config 
+
+    const [teachers, setTeachers] = useState([])
+    const textFieldRefFirst = useRef(null);
+    const textFieldRefLast = useRef(null);
+    const textFieldRefClass = useRef(null);
+
+    useEffect(() => {
+        const teachers = []
+        getDocs(collection(db, "Teachers")) //get collection
+        .then((allTeachers) => { //format each teacher into an array
+            allTeachers.forEach((teacher) => teachers.push({ id: teacher.id, ...teacher.data() }))
+        })
+    }, [db])
+
+    const deleteTeacher = (teacherID) => {
+        deleteDoc(doc(db, "Teachers", teacherID))
+        .then((docRef) => {
+            const updatedTeachers = [...teachers]
+            updatedTeachers.forEach((teacher) => {
+                console.log("teacherid: "+teacher.id)
+            })
+            setTeachers(updatedTeachers)
+        })
+        .catch((e) => console.error(e))
+    }
+
+    const addTeacher = (e) => {
+        e.preventDefault(); //no reloading the page
+
+        const newTeacher = {
+            firstName: textFieldRefFirst.current.value,
+            lastName: textFieldRefLast.current.value,
+            className: textFieldRefClass.current.value
+        }
+        addDoc(collection(db, "Teachers"), newTeacher) //add new Teacher
+        .then((docRef) => {
+            setTeachers([...teachers, {id: docRef.id, ...newTeacher}]) //update state variable
+        })
+        .catch((e) => console.error(e))
+        textFieldRefFirst.current.value = ""
+        textFieldRefLast.current.value = ""
+        textFieldRefClass.current.value = ""
+    }
+
+    return (
+        <div className="App">
+            <form onSubmit={addTeacher}>
+                <label for="firstName">Enter Teacher's First Name: </label>
+                <input id="firstName" type="text" ref={textFieldRefFirst} />
+                <p></p>
+
+                <label for="lastName">Enter Teacher's Last Name: </label>
+                <input id="lastName" type="text" ref={textFieldRefLast} />
+                <p></p>
+
+                <label for="className">Enter Teacher's Class Name: </label>
+                <input id="className" type="text" ref={textFieldRefClass} />
+                <p></p>
+                <input type="submit" value="Add Teacher"/>
+            </form>
+
+            {teachers.map((teacher) => <AllTeachers key={teacher.id} id={teacher.id} firstName={teacher.firstName} lastName={teacher.lastName} className={teacher.className} deleteTeacher={deleteTeacher} teacher={teacher}/>)}
+
+        </div>
+    );
+}
+export default TeacherDashboard;
