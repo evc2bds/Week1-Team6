b757b2e83da9f204667456dd3fad9289cc5126bb
PretzelOptician <alekschultz1@gmail.com>
2022-05-24 15:40:27 -0400

    Header and dropdown first version complete

--- src/DropdownNavbar.js
+++ src/DropdownNavbar.js
@@ -1,21 +1,38 @@
 import React from 'react';
+import {FormControl, Select, InputLabel, MenuItem} from "@mui/material"; 
 
 class DropdownNavbar extends React.Component{
+    constructor(props) {
+        super(props); 
+        this.state = {
+            nextPageValue: "homeDashboard"
+        }
+    }
     render() {
-        let nextPage = "";
+        const handleChange = (event) => {
+            this.setState({
+                nextPageValue: event.target.value
+            });
+            console.log("changed to " + event.target.value);
+            console.log("Implement React Router"); 
+            //TODO: USE REACT ROUTER TO MOVE BETWEEN WEB PAGES
+        }
         return(
-            <div>
-                <FormControl fullWidth>
-                    <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            <div style={{padding: 10}}>
+                <FormControl sx={{ m: 1, minWidth: 100 }}>
+                    <InputLabel id="next-page-label">Navigate to Page</InputLabel>
                     <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        value={age}
-                        label="Age"
+                        labelId="next-page-label"
+                        id="next-page"
+                        value={this.state.nextPageValue}
+                        label="Navigate to Page"
                         onChange={handleChange}>
-                        <MenuItem value={10}>Ten</MenuItem>
-                        <MenuItem value={20}>Twenty</MenuItem>
-                        <MenuItem value={30}>Thirty</MenuItem>
+                        <MenuItem value={"homeDashboard"}>Main Page</MenuItem>
+                        <MenuItem value={"studentDirectory"}>Student Directory</MenuItem>
+                        <MenuItem value={"teacherDashboard"}>Teacher Dashboard</MenuItem>
+                        <MenuItem value={"classDashboard"}>Classes</MenuItem>
+                        <MenuItem value={"teacherDirectory"}>Teacher Directory</MenuItem>
+                        <MenuItem value={"eventCalendar"}>Event Calendar</MenuItem>
                     </Select>
                 </FormControl>
             </div>
--- src/TitleHeader.js
+++ src/TitleHeader.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from "./TJLogo.jpg"
-import DropdownNavbar from "./DropdownNavbar.js"
 
 class TitleHeader extends React.Component {
     render() {
@@ -8,7 +7,6 @@ class TitleHeader extends React.Component {
             <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
                 <img src={logo} alt={"Thomas Jefferson Elementary School"} style={{width: 180, height: "auto"}}/>
                 <h2 style={{fontFamily: "arial", fontSize: 64, textAlign: "center", fontStyle: "italic", color: "#080740", marginLeft: 20}}>TJ Elementary School Dashboard</h2>
-                <DropdownNavbar/>
             </div>
         );
     }
