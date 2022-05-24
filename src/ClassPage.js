5aa3225971ea65c67e62fcd866a28259961ff2cf
PretzelOptician <alekschultz1@gmail.com>
2022-05-24 17:10:26 -0400

    initial class page setup

--- src/ClassPage.js
+++ src/ClassPage.js
@@ -4,7 +4,7 @@ class ClassPage extends React.Component {
     render() {
         return(
             <div>
-                
+                <p>class page</p>
             </div>
         );
     }
