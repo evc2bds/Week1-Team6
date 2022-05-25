2d32c0ae9965c36bf0c11568bba6f2b3258a795a
PretzelOptician <alekschultz1@gmail.com>
2022-05-25 17:26:04 -0400

    Class page basically done, just need to revisit after class dashboard

--- src/RosterList.js
+++ src/RosterList.js
@@ -9,7 +9,7 @@ class RosterList extends React.Component{
         this.state = {
             allStudents: [],
             studentsLoaded: false, 
-            addingStudent: false
+            changedStudent: false
         }
     }
     componentDidMount() {
@@ -28,6 +28,29 @@ class RosterList extends React.Component{
         }); 
         }); 
     }
+    componentDidUpdate() {
+        console.log("updating...")
+        console.log(this.state.changedStudent)
+        if(this.state.changedStudent) {
+            const q = collection(db, "students");
+            getDocs(q)
+            .then((allDocs) => {
+            let x = [];
+            allDocs.forEach((doc) => {
+                let newVar = doc.data(); 
+                newVar.id = doc.id; 
+                x.push(newVar)
+            })
+            .then(() => {this.setState({
+                allStudents: x, 
+                studentsLoaded: true,
+                changedStudent: false
+            })})
+            }); 
+            console.log("updated!");
+            console.log(this.state.changedStudent)
+        }
+    }
     addStudent = (student, currentRoster, classID) => {
         if(student) {
             let curRoster = currentRoster; 
@@ -37,15 +60,60 @@ class RosterList extends React.Component{
             // console.log(curRoster); 
             updateDoc(doc(db, "classes", ID), {
                 roster: curRoster
-            }).then(this.setState({ state: this.state }));
+            }).then(this.setState({ changedStudent: true }));
         }
+    }
+    getStudentID = (student) => {
+        let allStudentsInTheSchool = this.state.allStudents
+        for(let x = 0; x < allStudentsInTheSchool.length; x++) {
+            if(student.fname==allStudentsInTheSchool[x].fname && student.GPA==allStudentsInTheSchool[x].GPA && student.lname==allStudentsInTheSchool[x].lname) {
+                return allStudentsInTheSchool[x].id; 
+            }
+        }
+        return null; 
+    }
+    deleteStudent = (student, currentRoster, classID) => {
+        if(student) { 
+            const ID = classID;
+            const studentID = this.getStudentID(student); 
+            let newRoster = [];
+            for(let x = 0; x < currentRoster.length; x++) {
+                if(currentRoster[x].id!=studentID) {
+                    newRoster.push(currentRoster[x]);
+                }
+            }
+            updateDoc(doc(db, "classes", ID), {
+                roster: newRoster
+            }).then(this.setState({ changedStudent: true }));
+        }
+    }
+    equals(student1, student2) {
+        if(student1.GPA==student2.GPA && student1.fname==student2.fname && student1.lname==student2.lname) {
+            return true; 
+        }
+        else {return false;}
+    }
+    arrayIncludesValue(array, value) {
+        let bool = false; 
+        for(let x = 0; x < array.length; x++) {
+            if(this.equals(array[x], value)) {
+                return true; 
+            }
+        }
+        return bool; 
     }
     render() {
         const studentArray = this.props.studentRoster; 
+        for(let x = 0; x < studentArray.length; x++) {
+            if(!this.arrayIncludesValue(this.state.allStudents, studentArray[x])) {
+                studentArray.splice(x, 1);
+            }
+        }
         if(this.state.studentsLoaded){
             return(
                 <div style={{borderStyle: "dashed", borderWidth: 2, margin: 10}}>
-                    {studentArray.map((student) => <StudentBox  student={student}/>)}
+                    <p>{this.state.changedStudent.toString()}</p>
+                    {studentArray.map((student) => <StudentBox  classID={this.props.classID} rosterInfo={this.props.rosterInfo} deleteStudent={this.deleteStudent} key={student} student={student}/>)}
                     <h3 style={{textAlign: "left", marginLeft: 10}}>Add New Student: </h3>
                     <AddNewStudent classID={this.props.classID} rosterInfo={this.props.rosterInfo} addStudent={this.addStudent} allStudents={this.state.allStudents} currentStudents={studentArray}/>
                 </div>
@@ -54,7 +122,7 @@ class RosterList extends React.Component{
         else {
             return(
                 <div style={{borderStyle: "dashed", borderWidth: 2, margin: 10}}>
-                    {studentArray.map((student) => <StudentBox key={student} student={student}/>)}
+                    {studentArray.map((student) => <StudentBox classID={this.props.classID} rosterInfo={this.props.rosterInfo} addStudent={this.deleteStudent} key={student} student={student}/>)}
                 </div>
             );
         }
@@ -62,14 +130,17 @@ class RosterList extends React.Component{
 }
 
 class StudentBox extends React.Component {
+
     render() {
+        if(this.props.student) {
         return(
             <div style={{borderStyle: "solid", borderWidth: 1, margin: 4, marginRight: 8, marginLeft: 8}}>
                 <h5 style={{textAlign: "left", fontSize: 20, margin: 10}}>{this.props.student.fname + " " + this.props.student.lname}</h5>
                 <p style={{textAlign: "left", fontSize: 16, margin: 40}}>GPA: {this.props.student.GPA}</p>
-                <Button variant="outlined" color="error" style={{alignContent: "right", marginBottom: 10}} onClick={console.log("lol")}>Remove from Class</Button>
+                <Button variant="outlined" color="error" style={{alignContent: "right", marginBottom: 10}} onClick={() => this.props.deleteStudent(this.props.student, this.props.rosterInfo, this.props.classID)}>Remove from Class</Button>
             </div>
         );
+        }   
     }
 }
 
@@ -103,13 +174,6 @@ class AddNewStudent extends React.Component {
     render() {
         const allStudentArray = this.props.allStudents; 
         const curStudentArray = this.props.currentStudents; 
-        console.log(allStudentArray); 
-        console.log(curStudentArray); 
-        console.log(this.arrayIncludesValue(curStudentArray, {
-            "GPA": 3.3,
-            "fname": "Jason",
-            "lname": "Smith"
-        }));
         let notInClass = [];
         for(let x = 0; x < allStudentArray.length; x++) {
             if(!this.arrayIncludesValue(curStudentArray, allStudentArray[x])) {
